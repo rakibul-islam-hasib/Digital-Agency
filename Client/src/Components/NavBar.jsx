2d9e1e9ff2277e488ca5f598508c7f2f048cfaf9
Hasib Hossain <boraborhasib@gmail.com>
2023-04-27 21:13:42 +0600

    NavBar updated

--- Client/src/Components/NavBar.jsx
+++ Client/src/Components/NavBar.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
 
 const NavBar = () => {
+    const portfolioNavbar = [
+        {
+            label: 'Home',
+            url: '/',
+        },
+        {
+            label: 'About',
+            url: '/about',
+        },
+        {
+            label: 'Portfolio',
+            url: '/portfolio',
+        },
+        {
+            label: 'Services',
+            url: '/services',
+        },
+        {
+            label: 'Testimonials',
+            url: '/testimonials',
+        },
+        {
+            label: 'Blog',
+            url: '/blog',
+        },
+        {
+            label: 'Contact',
+            url: '/contact',
+        },
+        {
+            label: 'FAQ',
+            url: '/faq',
+        }
+    ];
+
     return (
-        <div className='w-[90%] mx-auto py-6'>
+        <div className='w-[88%] flex justify-between items-center mx-auto py-6'>
             <div className="logo">
                 <h1 className='text-3xl text-white font-bold'>R<span className='text-[#2AAFC0]'>.</span></h1>
+            </div>
+            <div className="text">
+                <ul className='flex justify-between text-white text-sm'>
+                    {portfolioNavbar.map((item, index) => (
+                        <li className='mx-5 text-[16px] font-semibold' key={index}>
+                            {item.label}
+                        </li>))}
+                </ul>
+
             </div>
         </div>
     );
